929019304a3df27ee8f82e965d1864d8a13d01db
mateuszanella <mateusblk1@gmail.com>
2025-02-25 19:59:03 -0300

    Initial setup

---
+++ tests/MainTest.php
@@ -0,0 +1,10 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class MainTest extends TestCase
+{
+    public function testHelloWorld()
+    {
+        $this->assertEquals('Hello, World!', 'Hello, World!');
+    }
+}
