5ddc22615cea2992d7647d93ca01579d496df5a0
mateuszanella <mateusblk1@gmail.com>
2025-03-10 20:40:37 -0300

    Working empty zip

--- src/Writers/Zip/Records/EndOfCentralDirectoryRecord.php
+++ src/Writers/Zip/Records/EndOfCentralDirectoryRecord.php
@@ -6,22 +6,28 @@ use PhpArchiveStream\Writers\Zip\Records\Fields\Packer;
 use PhpArchiveStream\Writers\Zip\Records\Fields\U16Field;
 use PhpArchiveStream\Writers\Zip\Records\Fields\U32Field;
 
-// i will create an empty zip
 class EndOfCentralDirectoryRecord
 {
     public const SIGNATURE = 0x06054b50;
 
-    public static function generate(): string
-    {
+    public static function generate(
+        int     $diskNumber,
+        int     $diskStart,
+        int     $numberOfCentralDirectoryRecords,
+        int     $totalCentralDirectoryRecords,
+        int     $centralDirectorySize,
+        int     $centralDirectoryOffset,
+        ?string $comment = ''
+    ): string {
         return Packer::pack(
             U32Field::create(self::SIGNATURE),
-            U16Field::create(0),
-            U16Field::create(0),
-            U16Field::create(0),
-            U16Field::create(0),
-            U32Field::create(0),
-            U32Field::create(0),
-            U16Field::create(0),
-        );
+            U16Field::create($diskNumber),
+            U16Field::create($diskStart),
+            U16Field::create($numberOfCentralDirectoryRecords),
+            U16Field::create($totalCentralDirectoryRecords),
+            U32Field::create($centralDirectorySize),
+            U32Field::create($centralDirectoryOffset),
+            U16Field::create(strlen($comment)),
+        ) . $comment;
     }
 }
--- src/Writers/Zip/Records/LocalFileHeader.php
+++ src/Writers/Zip/Records/LocalFileHeader.php
@@ -2,15 +2,37 @@
 
 namespace PhpArchiveStream\Writers\Zip\Records;
 
+use PhpArchiveStream\Writers\Zip\Records\Fields\U16Field;
+use PhpArchiveStream\Writers\Zip\Records\Fields\U32Field;
+
 class LocalFileHeader
 {
     public const SIGNATURE = 0x04034b50;
 
-    public static function get(
-
+    public static function generate(
+        int     $minimumVersion,
+        int     $generalPurposeBitFlag,
+        int     $compressionMethod,
+        int     $lastModificationTime,
+        int     $lastModificationDate,
+        int     $crc32,
+        int     $compressedSize,
+        int     $uncompressedSize,
+        string  $fileName,
+        ?string $extraField = ''
     ): string {
         return Packer::pack(
-            // Field::create('V', self::SIGNATURE),
-        );
+            U32Field::create(self::SIGNATURE),
+            U16Field::create($minimumVersion),
+            U16Field::create($generalPurposeBitFlag),
+            U16Field::create($compressionMethod),
+            U16Field::create($lastModificationTime),
+            U16Field::create($lastModificationDate),
+            U32Field::create($crc32),
+            U32Field::create($compressedSize),
+            U32Field::create($uncompressedSize),
+            U16Field::create(strlen($fileName)),
+            U16Field::create(strlen($extraField)),
+        ) . $fileName . $extraField;
     }
 }
