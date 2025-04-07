1e769b61096398cb052fb152dd85da4db405906c
mateuszanella <mateusblk1@gmail.com>
2025-04-07 20:58:28 -0300

    Added feature tests

--- tests/Feature/ZipTest.php
+++ tests/Feature/ZipTest.php
@@ -2,17 +2,17 @@
 
 namespace Tests\Feature;
 
-use PhpArchiveStream\Archives\Tar;
-use PhpArchiveStream\IO\Output\TarOutputStream;
-use PhpArchiveStream\Writers\Tar\TarWriter;
+use PhpArchiveStream\Archives\Zip;
+use PhpArchiveStream\IO\Output\OutputStream;
+use PhpArchiveStream\Writers\Zip\ZipWriter;
 use PHPUnit\Framework\TestCase;
 
 /**
- * @covers \PhpArchiveStream\Archives\Tar
+ * @covers \PhpArchiveStream\Archives\Zip
  */
-class TarTest extends TestCase
+class ZipTest extends TestCase
 {
-    protected string $outputPath = './output.tar';
+    protected string $outputPath = './output.zip';
     protected string $inputPath1 = './input1.txt';
     protected string $inputPath2 = './input2.txt';
 
@@ -52,12 +52,12 @@ class TarTest extends TestCase
     public function testAddFileFromPath()
     {
         $stream = fopen($this->outputPath, 'w');
-        $outputStream = new TarOutputStream($stream);
-        $tarWriter = new TarWriter($outputStream);
-        $tar = new Tar($tarWriter);
+        $outputStream = new OutputStream($stream);
+        $zipWriter = new ZipWriter($outputStream);
+        $zip = new Zip($zipWriter);
 
-        $tar->addFileFromPath('input1.txt', $this->inputPath1);
-        $tar->finish();
+        $zip->addFileFromPath('input1.txt', $this->inputPath1);
+        $zip->finish();
 
         $this->assertFileExists($this->outputPath);
     }
@@ -65,13 +65,13 @@ class TarTest extends TestCase
     public function testAddFileFromStream()
     {
         $stream = fopen($this->outputPath, 'w');
-        $outputStream = new TarOutputStream($stream);
-        $tarWriter = new TarWriter($outputStream);
-        $tar = new Tar($tarWriter);
+        $outputStream = new OutputStream($stream);
+        $zipWriter = new ZipWriter($outputStream);
+        $zip = new Zip($zipWriter);
 
         $resource = fopen($this->inputPath1, 'r');
-        $tar->addFileFromStream('input1.txt', $resource);
-        $tar->finish();
+        $zip->addFileFromStream('input1.txt', $resource);
+        $zip->finish();
 
         $this->assertFileExists($this->outputPath);
     }
@@ -79,12 +79,12 @@ class TarTest extends TestCase
     public function testAddFileFromContentString()
     {
         $stream = fopen($this->outputPath, 'w');
-        $outputStream = new TarOutputStream($stream);
-        $tarWriter = new TarWriter($outputStream);
-        $tar = new Tar($tarWriter);
+        $outputStream = new OutputStream($stream);
+        $zipWriter = new ZipWriter($outputStream);
+        $zip = new Zip($zipWriter);
 
-        $tar->addFileFromContentString('input1.txt', 'Hello World 1');
-        $tar->finish();
+        $zip->addFileFromContentString('input1.txt', 'Hello World 1');
+        $zip->finish();
 
         $this->assertFileExists($this->outputPath);
     }
@@ -92,15 +92,15 @@ class TarTest extends TestCase
     public function testFinish()
     {
         $stream = fopen($this->outputPath, 'w');
-        $outputStream = new TarOutputStream($stream);
-        $tarWriter = new TarWriter($outputStream);
-        $tar = new Tar($tarWriter);
+        $outputStream = new OutputStream($stream);
+        $zipWriter = new ZipWriter($outputStream);
+        $zip = new Zip($zipWriter);
 
-        $tar->finish();
+        $zip->finish();
 
-        $reflection = new \ReflectionClass($tar);
+        $reflection = new \ReflectionClass($zip);
         $property = $reflection->getProperty('writer');
         $property->setAccessible(true);
-        $this->assertNull($property->getValue($tar));
+        $this->assertNull($property->getValue($zip));
     }
 }
