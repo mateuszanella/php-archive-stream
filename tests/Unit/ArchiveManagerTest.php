093a4d0784ae7c56934b5c0eb52a2fd4a1f9ebbf
mateuszanella <mateusblk1@gmail.com>
2025-06-03 19:38:11 -0300

    Added basic tests

---
+++ tests/Unit/ArchiveManagerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Unit;
+
+use PhpArchiveStream\ArchiveManager;
+use PhpArchiveStream\Archives\Tar;
+use PhpArchiveStream\Config;
+use PHPUnit\Framework\TestCase;
+
+class ArchiveManagerTest extends TestCase
+{
+    public function testArchiveManagerInitialization(): void
+    {
+        $manager = new ArchiveManager;
+
+        $this->assertInstanceOf(ArchiveManager::class, $manager);
+        $this->assertInstanceOf(Config::class, $manager->config());
+    }
+
+    public function testAlias(): void
+    {
+        $manager = new ArchiveManager;
+
+        $manager->alias('tgz', 'tar.gz');
+
+        $archive = $manager->create('test.tgz', 'tgz');
+
+        $this->assertInstanceOf(Tar::class, $archive);
+        $this->assertFileExists('test.tgz');
+
+        $archive->finish();
+
+        @ unlink('test.tgz');
+    }
+}
